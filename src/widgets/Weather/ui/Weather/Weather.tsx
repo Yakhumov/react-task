e92d3d90530fb70f189f76193421cc8a7b0dec0f
Yakhumov <dgahar.yakumov@gmail.com>
2025-02-18 17:57:22 +0300

    fixes bag

--- src/widgets/Weather/ui/Weather/Weather.tsx
+++ src/widgets/Weather/ui/Weather/Weather.tsx
@@ -4,14 +4,8 @@ import { fetchweather } from "../../../../entities/Weather/model/slices/WeatherS
 import { RootState } from "../../../../app/providers/store/store";
 import { SearchBar } from "../../../Search";
 
-
-const clear_icon = new URL("../assets/clear.png", import.meta.url).href;
 const humidity_icon = new URL("../assets/humidity.png", import.meta.url).href;
 const wind_icon = new URL("../assets/wind.png", import.meta.url).href;
-const cloud_icon = new URL("../assets/cloud.png", import.meta.url).href;
-const drizzle_icon = new URL("../assets/drizzle.png", import.meta.url).href;
-const rain_icon = new URL("../assets/rain.png", import.meta.url).href;
-const show_icon = new URL("../assets/show.png", import.meta.url).href;
 
 export const Weather = () => {
   const dispatch = useAppDispatch();
@@ -19,23 +13,6 @@ export const Weather = () => {
     (state: RootState) => state.weather
   );
   const history = useSelector((state: RootState) => state.weather.savedHistory);
-
-  const allIcons: Record<string, string> = {
-    "01d": clear_icon,
-    "01n": clear_icon,
-    "02d": cloud_icon,
-    "02n": clear_icon,
-    "03d": cloud_icon,
-    "03n": cloud_icon,
-    "04d": drizzle_icon,
-    "04n": drizzle_icon,
-    "09d": rain_icon,
-    "09n": rain_icon,
-    "10d": rain_icon,
-    "10n": rain_icon,
-    "13d": show_icon,
-    "13n": show_icon,
-  };
 
   return (
     <div className="weather">
@@ -46,11 +23,7 @@ export const Weather = () => {
         <p>Ошибка: {error}</p>
       ) : data ? (
         <>
-          <img
-            src={allIcons[data?.icon] || clear_icon}
-            alt=""
-            className="weather-icon"
-          />
+          <img src={data.icon} alt="" className="weather-icon" />
           <p className="temperature">{data.temperature}°C</p>
           <p className="location">{data.location}</p>
           <div className="weather-data">
